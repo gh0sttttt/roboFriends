f251c764b2780631f30cf9bac5e7f30f61ba6bab
Aaron A. Abreu <a.modernfossil@gmail.com>
2020-09-10 17:03:09 -0400

    Updates

---
+++ precache-manifest.d2305b93b99c6de0ad9176f487372ad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30bd47a6eae6cb6bce9e1a56c7f754b",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/roboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "23561a53343ca01c2398",
+    "url": "/roboFriends/static/css/main.e4f50d74.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/roboFriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23561a53343ca01c2398",
+    "url": "/roboFriends/static/js/main.da7d553d.chunk.js"
+  },
+  {
+    "revision": "b20617b5161ccb3124b5",
+    "url": "/roboFriends/static/js/runtime-main.e4e9662b.js"
+  }
+]);
