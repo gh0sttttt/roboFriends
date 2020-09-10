dd1cfc59ae410c8652faaafe5bcae429385a427e
Aaron A. Abreu <a.modernfossil@gmail.com>
2020-09-10 16:53:49 -0400

    First commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    username: 'Lars',
+    email: 'lars@gmail.com'
+  },
+  {
+    id: 2,
+    username: 'Coral',
+    email: 'Coral@gmail.com'
+  },
+  {
+    id: 3,
+    username: 'Moby',
+    email: 'MobyD@gmail.com'
+  },
+  {
+    id: 4,
+    username: 'Issac',
+    email: 'Iamsic@gmail.com'
+  },
+  {
+    id: 5,
+    username: 'Sage',
+    email: 'SagePower@gmail.com'
+  },
+  {
+    id: 6,
+    username: 'Meadoww',
+    email: 'Mdow@gmail.com'
+  },
+  {
+    id: 7,
+    username: 'Todd',
+    email: 'TodoubleD@gmail.com'
+  },
+  {
+    id: 8,
+    username: 'Kane',
+    email: 'Kane@gmail.com'
+  },
+  {
+    id: 9,
+    username: 'Sam',
+    email: 'Sam@gmail.com'
+  },
+  {
+    id: 10,
+    username: 'Olo',
+    email: 'Olo@gmail.com'
+  },
+  {
+    id: 11,
+    username: 'Henrietta',
+    email: 'Hretta@gmail.com'
+  },
+  {
+    id: 12,
+    username: 'Gucci',
+    email: 'GucciThe@gmail.com'
+  },
+]
